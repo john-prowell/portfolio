de442be764594836976b4e8add019bcc2a870d2c
dtroxx <dtroxx@gmail.com>
2021-12-14 11:43:15 -0700

    added ACF for About Section Text

--- partials/home/about.php
+++ partials/home/about.php
@@ -12,17 +12,8 @@
       <div class="container about__container">
         <div class="row about__profile">
           <div class="col-12 about__profile-bio">            
-            <p class="about__profile-text">
-              I'm a full stack web developer currently living in Chandler, AZ.
-              who is available for remote or local work. Before becoming a
-              developer I was a graphic designer in the advertising specialties
-              industry. There I learned a lot about planning, design and keeping
-              things simple.</p>
-              <p>In my spare time I'm usually building things and
-              improving my skills. The MERN is where I've spent the bulk of my
-              time learning, but I'm always excited to learn new technologies!
-              Recently I have been focusing more on Digital Marketing and SEO.
-            </p>
+            <div class="about__profile-text"><?php the_field('about_section')?>
+            </div>
           </div>
         </div>        
       </div>
