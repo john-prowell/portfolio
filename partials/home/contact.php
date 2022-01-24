3578659755e022eb1c46c7abd44a14e33eb1f228
dtroxx <dtroxx@gmail.com>
2022-01-24 13:43:53 -0700

    updated contact section text

--- partials/home/contact.php
+++ partials/home/contact.php
@@ -4,7 +4,7 @@
   <div class="section__header">
     <h2 class="section__heading">Contact</h2>
     <div class="section__heading--sub">
-      Have a question or want to work together?
+    If you wanna get in touch regarding a project or full-time employment, please fill out the awesome form below.
     </div>
   </div>
 
