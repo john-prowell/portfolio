d31ea5b73c7c292353ddd28889f3f077195dde81
dtroxx <dtroxx@gmail.com>
2022-01-24 11:11:30 -0700

    updated scroll section top offset

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ function navHighlighter() {
   // Now we loop through sections to get height, top and ID values for each
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 100;
+    const sectionTop = current.offsetTop - 120;
     sectionId = current.getAttribute('id');
 
     /*
