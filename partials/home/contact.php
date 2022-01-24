1713873d2fa23949c5dd6ab392110517f5d197c6
dtroxx <dtroxx@gmail.com>
2022-01-24 13:45:05 -0700

    updated text

--- partials/home/contact.php
+++ partials/home/contact.php
@@ -4,33 +4,12 @@
   <div class="section__header">
     <h2 class="section__heading">Contact</h2>
     <div class="section__heading--sub">
-    If you wanna get in touch regarding a project or full-time employment, please fill out the awesome form below.
+    If you wanna get in touch regarding a project or full-time employment, please fill out the awesome form below. * Remote work preferred.
     </div>
   </div>
 
   <div class="container contact__container">
     <div class="row">
-      <!-- <div class="col-md-4">
-        <div class="contact__info">
-          <h3 class="contact__heading">Let's get in touch!</h3>
-          <div class="contact__text">
-            <svg class="contact__icon">
-              <use xlink:href="img/sprite.svg#email"></use>
-            </svg>
-            <p class="contact__email">
-              john@prowell.digital
-            </p>
-          </div>
-          <div class="contact__text">
-            <svg class="contact__icon">
-              <use xlink:href="img/sprite.svg#location"></use>
-            </svg>
-            <p class="contact__location">Chandler, AZ</p>
-          </div>
-          <span>* Remote work preferred</span>
-        </div>
-      </div> -->
-
       <div class="col-md-8 offset-md-2 contact__form">
         <?php $code = get_field('contact_code');
         echo do_shortcode($code);
