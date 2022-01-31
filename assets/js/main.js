88a3ba80231c77bafbc25f2a5aa25c18c581e164
dtroxx <dtroxx@gmail.com>
2022-01-31 09:09:26 -0700

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@ function navHighlighter() {
   // Now we loop through sections to get height, top and ID values for each
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 120;
+    const sectionTop = current.offsetTop - 100;
     sectionId = current.getAttribute('id');
 
     /*
