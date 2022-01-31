35350ef3c338aab80e7c129ca3065ce58d85fabb
dtroxx <dtroxx@gmail.com>
2022-01-31 09:22:55 -0700

    added smoothscroll back

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ add_action('wp_enqueue_scripts', 'load_styles');
 function load_scripts() {   
 
 		// Smooth Scroll
-    //wp_enqueue_script('smooth_scroll_scripts', get_template_directory_uri() . '/assets/js/smooth-scroll.polyfills.js', array('jquery'), '16.1.4', true);
+    wp_enqueue_script('smooth_scroll_scripts', get_template_directory_uri() . '/assets/js/smooth-scroll.polyfills.js', array('jquery'), '16.1.4', true);
 		// TwentyTwenty Image Comparison Slider		
 		wp_enqueue_script('jquery-event-move_scripts', get_template_directory_uri() . '/assets/js/jquery.event.move.js', array('jquery'), '2.0', true);
 		wp_enqueue_script('twentytwenty_scripts', get_template_directory_uri() . '/assets/js/jquery.twentytwenty.js', array('jquery'), '1.0', true);
