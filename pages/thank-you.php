ff34b6cee09eb483a7e7d90408f397c94731abf1
dtroxx <dtroxx@gmail.com>
2022-06-22 11:08:17 -0700

    added form thank you

---
+++ pages/thank-you.php
@@ -0,0 +1,10 @@
+<!-- Contact Thank You Page -->
+
+<?php
+get_header(); ?>
+
+<h1>Thank you! Your Awesome! <i>Seriously.</i></h1>
+<p>I've received your message and will respond to you as soon as possible</p>
+
+<?php get_footer();
+?>
