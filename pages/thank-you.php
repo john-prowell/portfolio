2763099b5da1c8ae8044a1cd020a4488ed5dda9d
dtroxx <dtroxx@gmail.com>
2022-06-23 17:57:18 -0700

    wrapped page in content div

--- pages/thank-you.php
+++ pages/thank-you.php
@@ -5,8 +5,14 @@
 */ 
 get_header(); ?>
 
+ <!-- Content -->
+ <div class="content">
+
+
 <h1>Thank you! Your Awesome! <i>Seriously.</i></h1>
 <p>I've received your message and will respond to you as soon as possible</p>
+
+</div><!-- /Content -->
 
 <?php get_footer();
 ?>
