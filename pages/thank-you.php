9c629202519066f5a5cfc2f837064e7cc3ac6293
dtroxx <dtroxx@gmail.com>
2022-06-23 18:12:44 -0700

    update

--- pages/thank-you.php
+++ pages/thank-you.php
@@ -6,11 +6,17 @@
 get_header(); ?>
 
  <!-- Content -->
- <div class="content">
-   <div class="thank-you">
-<h1>Thank you! Your Awesome! <i>Seriously.</i></h1>
-<p>I've received your message and will respond to you as soon as possible</p>
-</div>
+ <div class="content">   
+   <section class="thank-you">
+     <div class="container">
+       <div class="row">
+         <div class="col-12">
+           <h1>Thank you! Your Awesome! <i>Seriously.</i></h1>
+           <p>I've received your message and will respond to you as soon as possible</p>
+          </div>
+        </div>
+     </div>
+</section>
 
 </div><!-- /Content -->
 
