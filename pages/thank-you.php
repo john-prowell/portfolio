b5aa2000555b55b47006e8c6e3609bff2b31ea5b
dtroxx <dtroxx@gmail.com>
2022-06-23 17:58:56 -0700

    update

--- pages/thank-you.php
+++ pages/thank-you.php
@@ -7,12 +7,14 @@ get_header(); ?>
 
  <!-- Content -->
  <div class="content">
-
-
+   <div class="thank-you">
 <h1>Thank you! Your Awesome! <i>Seriously.</i></h1>
 <p>I've received your message and will respond to you as soon as possible</p>
+</div>
 
 </div><!-- /Content -->
+
+
 
 <?php get_footer();
 ?>
