2e358f0123fa668e9cca1d51a13206cb75e9f059
dtroxx <dtroxx@gmail.com>
2022-06-24 19:23:21 -0700

    added contact form thank you page

--- pages/thank-you.php
+++ pages/thank-you.php
@@ -7,15 +7,29 @@ get_header(); ?>
 
  <!-- Content -->
  <div class="content">   
-   <section class="thank-you">
+   <section class="thank-you hero">
      <div class="container">
        <div class="row">
          <div class="col-12">
-           <h1>Thank you! Your Awesome! <i>Seriously.</i></h1>
-           <p>I've received your message and will respond to you as soon as possible</p>
+         <div class="hero__content">                
+          <h1 class="hero__heading">John Prowell</h1>
+          <h3 class="hero__heading--sub">
+          <?php the_field('hero_h3')?>
+          </h3>          
+        </div>
+      </div>
+     </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <div class="thanks-message">
+           <h2 class="text-center">Thank you!</h2>
+           <p class="text-center">I've received your message and will respond shortly.</p>
           </div>
         </div>
-     </div>
+      </div>
+    </div>
 </section>
 
 </div><!-- /Content -->
