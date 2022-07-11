f4b52609d3c56e22dcbbbec5d1c928339f1ccf41
dtroxx <dtroxx@gmail.com>
2022-07-11 10:53:38 -0700

    updates

--- 404.php
+++ 404.php
@@ -8,11 +8,13 @@
 get_header(); ?>
 
 <div class="content">
-	<div class="container error-page">
+	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<h1>404</h1>
-				<p>Sorry, nothing to see here. Please click <a href="<?php get_site_url(); ?>">here</a> to get you back home!</p>
+				<div class="error-page">
+					<h1>404</h1>
+					<p>Sorry, nothing to see here. Please click <a href="<?php get_site_url(); ?>">here</a> to get you back home!</p>
+				</div>
 			</div>
 		</div>
 	</div>
