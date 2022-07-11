e992c42bcbd1db7c38f7d26ff6aafbd759f1faf4
dtroxx <dtroxx@gmail.com>
2022-07-11 10:12:57 -0700

    updated text

--- partials/home/portfolio.php
+++ partials/home/portfolio.php
@@ -20,7 +20,7 @@ $the_query = new WP_Query( $args ); ?>
 <?php if ( $the_query->have_posts() ) : ?>
   <div class="container portfolio__container">
     <div class="row">
-      <h3 class="text-center my-3">Web Design & Development</h3>
+      <h3 class="text-center my-3">Web Development</h3>
         <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
           <div class="col-12 col-md-6 col-lg-4 my-3">      
             <div class="card mx-auto h-100">
@@ -128,7 +128,7 @@ $graphic_query = new WP_Query( $args ); ?>
               <img src="<?php the_field('after_image') ?>" />
             </div>						
             <div class="card-body">
-            <h5 class="card-title text-center">Recreation to Vector</h5>
+            <h5 class="card-title text-center">Raster Recreation to Vector</h5>
           </div>
         </div>
        </div>
