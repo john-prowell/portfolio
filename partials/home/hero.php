1db397452378c7eddbec1474586e59ddeba3322c
dtroxx <dtroxx@gmail.com>
2022-08-16 11:31:13 -0700

    resolved conflict

--- partials/home/hero.php
+++ partials/home/hero.php
@@ -3,14 +3,9 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <div class="hero__content">                
-<<<<<<< HEAD
-          <h1 class="hero__heading" data-aos="zoom-in-left" data-aos-duration="1500">John Prowell</h1>
-          <h3 class="hero__heading--sub" data-aos="zoom-in-right" data-aos-duration="1500">
-=======
+        <div class="hero__content"> 
           <h1 class="hero__heading" data-aos="zoom-in-left" data-aos-duration="1000">John Prowell</h1>
           <h3 class="hero__heading--sub" data-aos="zoom-in-right" data-aos-duration="1000">
->>>>>>> 414501afcde763dd9c195fc7c85ea98c963a53da
           <?php the_field('hero_h3')?>
           </h3>
           <a href="#contact" class="btn-blue" data-aos="zoom-in-up" data-aos-duration="1500">Contact Now</a>          
