2dc2e87ee3b7577eacad8c5253724c50ef821426
dtroxx <dtroxx@gmail.com>
2022-08-16 11:26:48 -0700

    added aos animation

--- partials/home/hero.php
+++ partials/home/hero.php
@@ -4,11 +4,11 @@
     <div class="row">
       <div class="col-12">
         <div class="hero__content">                
-          <h1 class="hero__heading">John Prowell</h1>
-          <h3 class="hero__heading--sub">
+          <h1 class="hero__heading" data-aos="zoom-in-left" data-aos-duration="1500">John Prowell</h1>
+          <h3 class="hero__heading--sub" data-aos="zoom-in-right" data-aos-duration="1500">
           <?php the_field('hero_h3')?>
           </h3>
-          <a href="#contact" class="btn-blue">Contact Now</a>          
+          <a href="#contact" class="btn-blue" data-aos="zoom-in-up" data-aos-duration="1500">Contact Now</a>          
         </div>        
       </div>
     </div>
