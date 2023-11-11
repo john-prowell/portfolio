2793f49f00f9f1a93c96f04f76b3af6ae8cecd9a
john-prowell <dtroxx@gmail.com>
2023-11-11 14:49:13 -0700

    Hero Section Redesign

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
     </script>
 
     <title><?php echo get_bloginfo( 'name' );?> </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <?php wp_head(); ?>
   </head>
 
--- partials/home/hero.php
+++ partials/home/hero.php
@@ -1,16 +1,21 @@
 <!-- Hero Section -->
-<section id="hero" class="hero">   
+<section id="hero" class="hero">
   <div class="container">
     <div class="row">
-      <div class="col-12">
-        <div class="hero__content"> 
-          <h1 class="hero__heading" data-aos="zoom-in-left" data-aos-duration="1000">John Prowell</h1>
-          <h3 class="hero__heading--sub" data-aos="zoom-in-right" data-aos-duration="1000">
-          <?php the_field('hero_h3')?>
-          </h3>
-          <a href="#contact" class="btn-blue" data-aos="zoom-in-up" data-aos-duration="1500">Contact Now</a>          
+      <div class="col-12 col-lg-3 col-xl-5 d-flex align-items-center">
+        <div class="hero__photo">        
+          <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/assets/images/photo.jpg' ?>" alt="John Prowell"  data-aos="zoom-in-right" data-aos-duration="1000">            
+        </div>       
+      </div>
+
+      <div class="col-12 col-lg-9 col-xl-7 d-flex justify-content-center">
+        <div class="hero__content">
+          <span class="intro-text">Welcome to my world</span>
+          <h1 data-aos="zoom-in-left" data-aos-duration="1000">Hi, I’m John Prowell,<br> <span>Wordpress Developer</span><br>& Production Artist.</h1>          
+          <a href="#contact" class="contact" data-aos="zoom-in-up" data-aos-duration="1500"><i class="fa-regular fa-envelope fa-lg"></i> Let's Talk</a>
         </div>        
       </div>
+
     </div>
-  </div>    
+  </div>
 </section>
