07e8d13864f31c1fcc81578d463d0e71347beb35
john-prowell <dtroxx@gmail.com>
2023-11-25 12:57:57 -0700

    replaced bootstrap menu and other minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,14 +69,40 @@ jQuery(window).load(function () {
 
 AOS.init();
 
-// To close Boostrap Off Canvas menu when clicking on anchor links
+// --- Mobile Nav Menu
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont');
+const smallMenu = document.querySelector('.header__sm-menu');
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu');
+const headerHamMenuCloseBtn = document.querySelector(
+  '.header__main-ham-menu-close'
+);
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link');
 
-function close_offcanvas() {
-  var element1 = document.getElementById('eff_nav_toggler');
-  element1.classList.remove('collapsed');
-  var element2 = document.getElementById('navbarSupportedContent');
-  element2.classList.remove('show');
-  document
-    .getElementById('eff_nav_toggler')
-    .setAttribute('aria-expanded', 'false');
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active');
+  } else {
+    smallMenu.classList.add('header__sm-menu--active');
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none');
+    headerHamMenuCloseBtn.classList.add('d-none');
+  } else {
+    headerHamMenuBtn.classList.add('d-none');
+    headerHamMenuCloseBtn.classList.remove('d-none');
+  }
+});
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active');
+    headerHamMenuBtn.classList.remove('d-none');
+    headerHamMenuCloseBtn.classList.add('d-none');
+  });
 }
+
+const headerLogoContainer = document.querySelector('.header__logo-container');
+
+headerLogoContainer.addEventListener('click', () => {
+  location.href = '#';
+});
