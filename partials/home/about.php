0627965d747f09c031918280dfa7070d7fc1ea05
john-prowell <dtroxx@gmail.com>
2024-04-07 11:50:34 -0700

    changed to use acf for skill list

--- partials/home/about.php
+++ partials/home/about.php
@@ -11,30 +11,19 @@
       <!-- About Text -->
       <div class="col-md-6">
         <h3 class="mb-3">A brief intro...</h3>
-        <div class="about__text"><?php the_field('about_section')?></div>
+        <div class="about__text"><?php the_field('about_section') ?></div>
       </div>
-      <!-- Skills Lists with Icons -->     
+      <!-- Skills List -->
+      <?php $skills = get_field('skill_list'); ?>
       <div class="col-md-6">
-        <h3 class="mb-4">My Skills</h3>
-        <div class="about__skills">
-          <div class="about__skills-skill">HTML</div>
-          <div class="about__skills-skill">CSS</div>
-          <div class="about__skills-skill">SASS</div>
-          <div class="about__skills-skill">PHP</div>
-          <div class="about__skills-skill">JavaScript</div>
-          <div class="about__skills-skill">WordPress</div>
-          <div class="about__skills-skill">Bootstrap</div>
-          <div class="about__skills-skill">Responsive Design</div>
-          <div class="about__skills-skill">SEO</div>
-          <div class="about__skills-skill">Custom Themes</div>
-          <div class="about__skills-skill">Website Optimization</div>
-          <div class="about__skills-skill">Basic Plugins</div>
-          <div class="about__skills-skill">Github</div>
-          <div class="about__skills-skill">Photoshop</div>
-          <div class="about__skills-skill">Illustrator</div>
-          <div class="about__skills-skill">Raster to Vector Conversion</div>
-          <div class="about__skills-skill">Layout & Typesetting</div>
-        </div>
+        <?php if ($skills) { ?>
+          <h3 class="mb-4">My Skills</h3>
+          <div class="about__skills">
+            <?php foreach ($skills as $skill) { ?>
+              <div class="about__skills-skill"><?php echo $skill['skill_name'] ?></div>
+            <?php } ?>
+          </div>
+        <?php } ?>
       </div>
     </div>
   </div>
