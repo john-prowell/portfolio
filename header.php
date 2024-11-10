ed5bbc9c9fb487e6f55e242585988d146882ec91
john-prowell <dtroxx@gmail.com>
2024-11-10 15:47:58 -0700

    Updated title tag to be dynamic

--- header.php
+++ header.php
@@ -15,7 +15,8 @@
       gtag('config', 'G-YZ1TJC5J4H');
     </script>
 
-    <title><?php echo get_bloginfo( 'name' );?> </title>
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <?php wp_head(); ?>
   </head>
